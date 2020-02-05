d4c78eba45309c0909265a7b409ad08ad68e00b2
rebrin <rebrin53@gmail.com>
2020-02-05 07:44:02 -0600

    añadiendo botones con estilos e inconos svg

--- app/src/main/java/mx/edu/itsp/curso/constraint1_2020/MainActivity.kt
+++ app/src/main/java/mx/edu/itsp/curso/constraint1_2020/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 
 import androidx.annotation.RequiresApi
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,13 +23,19 @@ class MainActivity : AppCompatActivity() {
         toolbar.title="mi tool bar"
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val Fab=findViewById<FloatingActionButton>(R.id.FABAccion)
+        Fab.setOnClickListener({
+            Toast.makeText(applicationContext,"pulsado el boton FAB",
+                Toast.LENGTH_LONG).show()
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar,menu)
         return true
     }
-    
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         val id= item?.itemId
         when(id){
